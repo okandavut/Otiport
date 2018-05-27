437d8be7c5be5ba281e95dde01012663ebce337d
Barış Ceviz <me@barisceviz.com>
2018-05-28 01:16:45 +0300

    Added Entity Class Library

---
+++ MaviNokta.Entities/Users/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MaviNokta.Entities.Users
+{
+    public class UserEntity : BaseEntity<Guid>
+    {
+        public UserEntity()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int UserGroupId { get; set; }
+
+        //relations
+        public virtual UserGroupEntity UserGroup { get; set; }
+    }
+}
---
+++ MaviNokta.Entities/Users/UserGroupEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaviNokta.Entities.Users
+{
+    public class UserGroupEntity : BaseEntity<int>
+    {
+        public string Name { get; set; }
+
+        //relations
+        public virtual List<UserEntity> Users { get; set; }
+    }
+}
