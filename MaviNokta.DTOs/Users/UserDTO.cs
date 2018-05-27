4d88459ffeae91ca07a34181e342860511fe024d
Barış Ceviz <me@barisceviz.com>
2018-05-28 01:16:24 +0300

    Added DTO Class Library

---
+++ MaviNokta.DTOs/Users/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaviNokta.DTOs.Users
+{
+    public class UserDTO : BaseDTO<Guid>
+    {
+        public UserDTO()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public string Username { get; set; }
+    }
+}
