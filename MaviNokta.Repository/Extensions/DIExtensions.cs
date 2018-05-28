a4e55d928fa54e9e008bff0723923288ff8edd77
Barış Ceviz <me@barisceviz.com>
2018-05-28 23:38:12 +0300

    Added Extension Methods to Dependency Injection

---
+++ MaviNokta.Repository/Extensions/DIExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MaviNokta.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaviNokta
+{
+    public static class DIExtensions
+    {
+        public static void AddRepository(this IServiceCollection services,string connectionString)
+        {
+            services.AddDbContext<MaviNoktaDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString)
+            });
+        }
+    }
+}
