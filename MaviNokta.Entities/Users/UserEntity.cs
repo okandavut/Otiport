63cdb5bab540b547e60ce8eac51b614e97c5395f
Barış Ceviz <me@barisceviz.com>
2018-05-28 23:36:42 +0300

    Added Table Name

--- MaviNokta.Entities/Users/UserEntity.cs
+++ MaviNokta.Entities/Users/UserEntity.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MaviNokta.Entities.Users
 {
+    [Table("Users")]
     public class UserEntity : BaseEntity<Guid>
     {
         public UserEntity()
         {
             this.Id = Guid.NewGuid();
         }
-
+        
         public string Username { get; set; }
         public string Password { get; set; }
         public int UserGroupId { get; set; }
