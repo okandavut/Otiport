2cda26c60a003b96b7e240b9c1ed6b667c12084f
Barış Ceviz <me@barisceviz.com>
2018-05-28 22:50:39 +0300

    Added UserGroupDTO

---
+++ MaviNokta.DTOs/Users/UserGroupDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaviNokta.DTOs.Users
+{
+    public class UserGroupDTO : BaseDTO<int>
+    {
+        public string Name { get; set; }
+    }
+}
