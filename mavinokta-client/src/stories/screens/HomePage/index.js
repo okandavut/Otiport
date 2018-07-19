72abb905c3f446268de161d9b96f03370968e1c3
Barış Ceviz <me@barisceviz.com>
2018-07-19 23:29:56 +0300

    Added UI Screens

---
+++ mavinokta-client/src/stories/screens/HomePage/index.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export interface Props {
+  title: string;
+}
+export interface State {
+  count: number;
+}
+
+export default class HomePage extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return <div />;
+  }
+}
