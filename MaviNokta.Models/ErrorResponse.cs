f829483897d34fee7b64ce37fe2082b8f344b150
Barış Ceviz <me@barisceviz.com>
2018-07-19 23:28:07 +0300

    Added ErrorResponse Model

---
+++ MaviNokta.Models/ErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaviNokta.Models
+{
+    public class ErrorResponse
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public List<Error> Errors { get; set; } = new List<Error>();
+    }
+
+    public class Error
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+    }
+}
