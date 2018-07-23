409012131c0866e5622b25873fafbaa43d1c57e9
okandavut <davut.okan@gmail.com>
2018-07-23 15:45:24 +0300

    Test

--- MaviNokta.DTOs/ProfileItem/ProfileItemToUserDTO.cs
+++ MaviNokta.DTOs/ProfileItem/ProfileItemToUserDTO.cs
@@ -6,6 +6,6 @@ namespace MaviNokta.DTOs.Users {
     public class ProfileItemToUserDTO : BaseDTO<int> {
         public int ProfileItemId { get; set; }
         public int UserId { get; set; }
-
+ 
     }
 }
