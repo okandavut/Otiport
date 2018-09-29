692734574d3feced54cf34caab69f3f334425b31
Baris Ceviz <me@barisceviz.com>
2018-09-29 22:54:16 +0300

    Created token provider

---
+++ src/Otiport.API/Providers/ITokenProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Otiport.API.Providers
+{
+    public interface ITokenProvider
+    {
+        Task<string> GenerateTokenAsync(Guid userEntityId, string emailAddress, int userGroupId);
+    }
+}
---
+++ src/Otiport.API/Providers/TokenProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Otiport.API.Providers
+{
+    public class TokenProvider : ITokenProvider
+    {
+        public Task<string> GenerateTokenAsync(Guid userEntityId, string emailAddress, int userGroupId)
+        {
+            //TODO (peacecwz): Implement JWT token provider
+            return Task.FromResult("");
+        }
+    }
+}
