195d7219a43d7d2968aefd692c9700558eae98fc
Baris Ceviz <me@barisceviz.com>
2018-09-29 23:35:23 +0300

    Implemented API Services

---
+++ client/Otiport-client/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import endpoints from "./endpoints";
+import LoginRequest from "../models/request/users/loginRequest";
+import LoginResponse from "../models/response/users/loginResponse";
+export default class APIService {
+  client = null;
+  constructor() {
+    this.client = axios.create({
+      baseURL: endpoints.baseApiPath,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+
+  login(model: LoginRequest) {
+    return this.client.post(endpoints.login, model).then(res => new LoginResponse(res.json()));
+  }
+}
