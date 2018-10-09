6954657f756a3cc299d6ac1c6c9232f29b8c3c6d
Baris Ceviz <me@barisceviz.com>
2018-10-10 00:54:15 +0300

    Added route names

--- src/Otiport.API/Controllers/UsersController.cs
+++ src/Otiport.API/Controllers/UsersController.cs
@@ -13,15 +13,16 @@ namespace Otiport.API.Controllers
     public class UsersController : ApiControllerBase
     {
         private readonly IUserService _userService;
+
         public UsersController(IUserService userService)
         {
             _userService = userService;
         }
 
-        [ProducesResponseType(typeof(CreateUserResponse), (int)HttpStatusCode.Created)]
-        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
-        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
-        [HttpPost]
+        [ProducesResponseType(typeof(CreateUserResponse), (int) HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ErrorResponse), (int) HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(ErrorResponse), (int) HttpStatusCode.BadRequest)]
+        [HttpPost, Route("register")]
         public async Task<IActionResult> Create([FromBody] CreateUserRequest request)
         {
             var response = await _userService.CreateUserAsync(request);
@@ -30,17 +31,19 @@ namespace Otiport.API.Controllers
                 return BadRequest();
             }
 
-            return StatusCode((int)HttpStatusCode.Created, response);
+            return StatusCode((int) HttpStatusCode.Created, response);
         }
-        [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginRequest request) {
+
+        [HttpPost, Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
             var response = await _userService.LoginAsync(request);
             if (response == null)
             {
                 return BadRequest();
             }
 
-            return StatusCode((int)HttpStatusCode.OK, response);
+            return StatusCode((int) HttpStatusCode.OK, response);
         }
     }
 }
