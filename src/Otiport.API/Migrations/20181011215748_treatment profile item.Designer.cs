a82dc211d53bd307b0c21860f171156b03729afb
okandavutcom <davut.okan@gmail.com>
2018-10-12 00:58:48 +0300

    Treatments, profile items migration was added and database was generated.

--- src/Otiport.API/Migrations/20181011215748_treatment profile item.Designer.cs
+++ src/Otiport.API/Migrations/20181011215748_treatment profile item.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Otiport.API.Data;
 
 namespace Otiport.API.Migrations
 {
     [DbContext(typeof(OtiportDbContext))]
-    partial class OtiportDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181011215748_treatment profile item")]
+    partial class treatmentprofileitem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
