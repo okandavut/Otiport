62c7926e7328fb8679303e5e52d30be979474580
okandavutcom <davut.okan@gmail.com>
2018-10-14 01:25:22 +0300

    Default user group id was added configuration json

---
+++ src/Otiport.API/Common/Contants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Otiport.API.Common
+{
+    public class Contants
+    {
+        public const string DefaultUserGroupId = nameof(DefaultUserGroupId); 
+    }
+}
