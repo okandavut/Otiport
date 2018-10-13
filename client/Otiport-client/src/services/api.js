0c1ce15fca91f9c0bdc56852c0c2b7d2e9feb698
okandavutcom <davut.okan@gmail.com>
2018-10-14 01:23:26 +0300

    Create user api and endpoint settings were done.

--- client/Otiport-client/src/services/api.js
+++ client/Otiport-client/src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import endpoints from "./endpoints";
 import LoginRequest from "../models/request/users/loginRequest";
 import LoginResponse from "../models/response/users/loginResponse";
+import CreateUserRequest from "../models/request/users/createUserRequest";
+import CreateUserResponse from "../models/response/users/createUserResponse";
 class APIService {
   client = null;
   constructor() {
@@ -17,6 +19,12 @@ class APIService {
     return this.client
       .post(endpoints.login, model)
       .then(res => new LoginResponse(res.data));
+  }
+
+  createUser(model: CreateUserRequest) {
+    return this.client
+      .post(endpoints.createUser, model)
+      .then(res => new CreateUserResponse(res.data));
   }
 }
 
