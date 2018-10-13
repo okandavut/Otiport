993f9f6de7833ea15e2d326ca75041196987b810
okandavutcom <davut.okan@gmail.com>
2018-10-14 01:23:52 +0300

    create user ui and actions development was done.

--- client/Otiport-client/src/containers/CreateUserContainer/actions.js
+++ client/Otiport-client/src/containers/CreateUserContainer/actions.js
@@ -1,4 +1,7 @@
 import types from "../../common/types";
+import APIService from "../../services/api";
+import CreateUserRequest from "../../models/request/users/createUserRequest";
+import UserModel from "../../models/request/users/userModel";
 
 export function changeIsLoading(isLoading: boolean) {
   return {
@@ -7,12 +10,40 @@ export function changeIsLoading(isLoading: boolean) {
   };
 }
 
-export function createUser(name,surname,email) {
+export function createUser(
+  userName,
+  password,
+  emailAddress,
+  firstName,
+  middleName,
+  lastName,
+  birthDate,
+  country,
+  city,
+  district
+) {
   return dispatch => {
     dispatch(changeIsLoading(true));
-
-    //Call Api for register
-
+    APIService.createUser(
+      new CreateUserRequest({
+        userModel: new UserModel({
+          userName: userName,
+          password: password,
+          emailAddress: emailAddress,
+          firstName: firstName,
+          middleName: middleName,
+          lastName: lastName,
+          birthDate: birthDate,
+          country: country,
+          city: city,
+          district: district
+        })
+      })
+    ).then(res => {
+      if (!res.isSuccess) {
+        console.log("hataa");
+      }
+    });
     dispatch(changeIsLoading(false));
   };
 }
