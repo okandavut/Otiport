a9a272e1803b74a712e33705cb820d4573f39cd6
okandavutcom <davut.okan@gmail.com>
2018-11-04 10:17:29 +0300

    React project was deleted

--- client/Otiport-client/src/models/request/users/createUserRequest.js
+++
@@ -1,9 +0,0 @@
-import UserModel from "./userModel";
-
-export default class CreateUserRequest {
-  constructor(obj) {
-    this.userModel = obj.userModel;
-  }
-
-  userModel: UserModel;
-}
--- client/Otiport-client/src/models/response/users/createUserResponse.js
+++
@@ -1,7 +0,0 @@
-import ResponseBase from "../../responseBase";
-
-export default class CreateUserResponse extends ResponseBase {
-  constructor(obj) {
-    super(obj);
-  }
-}
