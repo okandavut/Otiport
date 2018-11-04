cc455076e067a7ddf73c662076306ad919ee33c5
okandavutcom <davut.okan@gmail.com>
2018-11-04 10:22:01 +0300

    Frontend Project was deleted

--- client/Otiport-client/src/services/endpoints.js
+++
@@ -1,9 +0,0 @@
-class Endpoints {
-  baseApiPath = "https://localhost:44398/";
-
-  users = this.baseApiPath + "/users";
-  login = this.users + "/login";
-  createUser = this.users + "/register";
-}
-
-export default new Endpoints();
