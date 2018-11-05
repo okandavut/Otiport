ad300e7235dad681dc8b246c8721951135ebe27a
okandavutcom <davut.okan@gmail.com>
2018-11-05 22:47:16 +0300

    GetDistricts  rquest and respone items were added.

---
+++ src/Otiport.API/Contract/Response/Common/GetDistrictsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Data.Entities.AddressInformations;
+
+namespace Otiport.API.Contract.Response.Common
+{
+    public class GetDistrictsResponse : ResponseBase
+    {
+        public IEnumerable<DistrictEntity> ListOfDistricts;
+    }
+}
