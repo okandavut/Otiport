d96681a038fe54ba4ea3fa1ab626abcc3003d399
PeaceCwz <me@barisceviz.com>
2018-11-10 14:52:19 +0300

    Changed variable name

--- client/Otiport-client/src/services/endpoints.js
+++ client/Otiport-client/src/services/endpoints.js
@@ -1,11 +1,11 @@
 class Endpoints {
-  baseApiPath = "https://localhost:44398/";
+  baseApiPath = "https://localhost:5001/";
 
   users = this.baseApiPath + "/users";
-  adress = this.baseApiPath + "/address-informations";
+  address = this.baseApiPath + "address-informations";
   login = this.users + "/login";
   createUser = this.users + "/register";
-  getCountries = this.adress + "/countries";
+  getCountries = this.address + "/countries";
 }
 
 export default new Endpoints();
