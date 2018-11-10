fa019d4b80615c49630099a2b3c0f724abb8d395
PeaceCwz <me@barisceviz.com>
2018-11-10 14:53:59 +0300

    Added state to reducer

--- client/Otiport-client/src/reducers/index.js
+++ client/Otiport-client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import HomePageReducer from "../containers/HomePageContainer/reducer";
-import CreateUserPageReducer from "../containers/HomePageContainer/reducer";
+import CreateUserPageReducer from "../containers/CreateUserContainer/reducer";
 import LoginPageReducer from "../containers/LoginPageContainer/reducer";
 export default combineReducers({
   homePageReducer: HomePageReducer,
