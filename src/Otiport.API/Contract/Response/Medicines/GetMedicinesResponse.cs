05d13ea144f31b2ae3122dbc100989fbbb590280
okandavutcom <davut.okan@gmail.com>
2019-01-05 00:54:29 +0300

    Request and response for medicine operations

---
+++ src/Otiport.API/Contract/Response/Medicines/GetMedicinesResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Data.Entities.Medicine;
+
+namespace Otiport.API.Contract.Response.Medicines
+{
+    public class GetMedicinesResponse : ResponseBase
+    {
+        public IEnumerable<MedicineEntity> ListOfMedicines { get; set; }
+    }
+}
