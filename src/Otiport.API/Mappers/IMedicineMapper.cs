5b4c803da3c63977ebe95cf69174a6b58cfacd27
okandavutcom <davut.okan@gmail.com>
2019-01-06 02:56:37 +0300

    Mapper addded for medicine

---
+++ src/Otiport.API/Mappers/IMedicineMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Contract.Models;
+using Otiport.API.Data.Entities.Medicine;
+using Otiport.API.Data.Entities.Users;
+
+namespace Otiport.API.Mappers
+{
+    public interface IMedicineMapper
+    {
+        MedicineModel ToModel(MedicineEntity entity);
+        MedicineEntity ToEntity(MedicineModel model);
+    }
+}
---
+++ src/Otiport.API/Mappers/Implementations/MedicineMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Contract.Models;
+using Otiport.API.Data.Entities.Medicine;
+
+namespace Otiport.API.Mappers.Implementations
+{
+    public class MedicineMapper : IMedicineMapper
+    {
+        public MedicineModel ToModel(MedicineEntity entity)
+        {
+            return new MedicineModel()
+            {
+                MedicineId = entity.Id,
+                Title = entity.Title,
+                Description = entity.Description
+            };
+        }
+
+        public MedicineEntity ToEntity(MedicineModel model)
+        {
+
+            return new MedicineEntity()
+            {
+                Id = model.MedicineId,
+                Title = model.Title,
+                Description = model.Description
+            };
+        }
+    }
+}
