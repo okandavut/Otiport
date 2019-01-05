8f1ec7442a7ade4f63fc88ea6859196b6b6fc8f7
okandavutcom <davut.okan@gmail.com>
2019-01-06 02:55:39 +0300

    Response changed

--- src/Otiport.API/Contract/Response/Medicines/GetMedicinesResponse.cs
+++ src/Otiport.API/Contract/Response/Medicines/GetMedicinesResponse.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Otiport.API.Contract.Models;
 using Otiport.API.Data.Entities.Medicine;
 
 namespace Otiport.API.Contract.Response.Medicines
 {
     public class GetMedicinesResponse : ResponseBase
     {
-        public IEnumerable<MedicineEntity> ListOfMedicines { get; set; }
+        public IEnumerable<MedicineModel> Medicines { get; set; }
     }
 }
