c1fd746113a9719cc58784a10848e63d9e486852
okandavutcom <davut.okan@gmail.com>
2019-01-06 02:56:11 +0300

    generate response added

--- src/Otiport.API/Helpers/ApiControllerBase.cs
+++ src/Otiport.API/Helpers/ApiControllerBase.cs
@@ -1,7 +1,15 @@
-﻿namespace Otiport.API.Helpers
+﻿using Microsoft.AspNetCore.Mvc;
+using Otiport.API.Contract;
+
+namespace Otiport.API.Helpers
 {
     public class ApiControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
     {
-        
+        [NonAction]
+        public IActionResult GenerateResponse(ResponseBase response)
+        {
+            return StatusCode(response.StatusCode, response);
+        }
+
     }
 }
