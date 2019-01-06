0a15b22cbad2fd98bc20b09f0b8a9c7457c5a66f
okandavutcom <davut.okan@gmail.com>
2019-01-06 22:44:25 +0300

    Treatment request and responses

---
+++ src/Otiport.API/Contract/Request/Treatments/UpdateTreatmentRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Otiport.API.Contract.Request.Treatments
+{
+    public class UpdateTreatmentRequest : RequestBase
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Otiport.API/Contract/Response/Treatments/GetTreatmentsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Contract.Models;
+
+namespace Otiport.API.Contract.Response.Treatments
+{
+    public class GetTreatmentsResponse : ResponseBase
+    {
+        public IEnumerable<TreatmentModel> Treatments { get; set; }
+    }
+}
