663ae4bf4d1fed083b7c5804acdde1104cc3a2c0
okandavutcom <davut.okan@gmail.com>
2019-01-06 22:44:49 +0300

    Treatment mappers

---
+++ src/Otiport.API/Mappers/ITreatmentMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Contract.Models;
+using Otiport.API.Data.Entities.Treatment;
+
+namespace Otiport.API.Mappers
+{
+    public interface ITreatmentMapper
+    {
+        TreatmentModel ToModel(TreatmentEntity entity);
+        TreatmentEntity ToEntity(TreatmentModel model);
+    }
+}
