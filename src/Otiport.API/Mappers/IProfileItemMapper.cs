bd78278b8e84f16dc987c7b65c459812ec702c89
okandavutcom <davut.okan@gmail.com>
2019-01-13 23:31:55 +0300

    Profile ıtem operations mapper done

---
+++ src/Otiport.API/Mappers/IProfileItemMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Contract.Models;
+using Otiport.API.Contract.Request.ProfileItems;
+using Otiport.API.Data.Entities.ProfileItem;
+
+namespace Otiport.API.Mappers
+{
+    public interface IProfileItemMapper
+    {
+        ProfileItemModel ToModel(ProfileItemEntity entity);
+        ProfileItemEntity ToEntity(ProfileItemModel model);
+        ProfileItemEntity ToEntity(AddProfileItemRequest request);
+    }
+}
---
+++ src/Otiport.API/Mappers/Implementations/ProfileItemMapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Otiport.API.Contract.Models;
+using Otiport.API.Contract.Request.ProfileItems;
+using Otiport.API.Data.Entities.ProfileItem;
+
+namespace Otiport.API.Mappers.Implementations
+{
+    public class ProfileItemMapper : IProfileItemMapper
+    {
+        public ProfileItemModel ToModel(ProfileItemEntity entity)
+        {
+            return new ProfileItemModel()
+            {
+                ProfileItemId = entity.Id,
+                Title = entity.Title,
+                Description = entity.Description
+            };
+        }
+
+        public ProfileItemEntity ToEntity(ProfileItemModel model)
+        {
+
+            return new ProfileItemEntity()
+            {
+                Id = model.ProfileItemId,
+                Title = model.Title,
+                Description = model.Description
+            };
+        }
+
+        public ProfileItemEntity ToEntity(AddProfileItemRequest request)
+        {
+            return new ProfileItemEntity()
+            {
+                Title = request.Title,
+                Description = request.Description
+            };
+        }
+    }
+}
