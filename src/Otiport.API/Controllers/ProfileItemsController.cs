fd686b29c2f6c7bfaecd6b16c6e40d8f396ec2d8
okandavutcom <davut.okan@gmail.com>
2019-01-13 23:31:25 +0300

    Profile item operations controller done

---
+++ src/Otiport.API/Controllers/ProfileItemsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Otiport.API.Contract.Request.ProfileItems;
+using Otiport.API.Contract.Response.ProfileItems;
+using Otiport.API.Helpers;
+using Otiport.API.Services;
+
+namespace Otiport.API.Controllers
+{
+    [Route("profile-items")]
+    public class ProfileItemsController : ApiControllerBase
+    {
+        private readonly IProfileItemService _profileItemService;
+
+        public ProfileItemsController(IProfileItemService profileItemService)
+        {
+            _profileItemService = profileItemService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProfileItem([FromBody] AddProfileItemRequest request)
+        {
+            var response = await _profileItemService.AddProfileItemAsync(request);
+            return GenerateResponse(response);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProfileItems(GetProfileItemsRequest request)
+        {
+            var response = await _profileItemService.GetProfileItemsAsync(request);
+            return GenerateResponse(response);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProfileItem([FromBody] DeleteProfileItemRequest request)
+        {
+            var response = await _profileItemService.DeleteProfileItemAsync(request);
+            return GenerateResponse(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProfileItem([FromRoute] int id, [FromBody] UpdateProfileItemRequest request)
+        {
+            request = request ?? new UpdateProfileItemRequest();
+            request.Id = id;
+            var response = await _profileItemService.UpdateProfileItemAsync(request);
+            return GenerateResponse(response);
+
+        }
+    }
+}
